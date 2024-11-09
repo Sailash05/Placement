2e41de3fefba5b95594acf4649a53e39a6b02287
Sailash2005 <s.sailash2005@gmail.com>
2024-11-09 17:30:57 +0530

    second commit

---
+++ src/main/java/com/placementserver/placementserver/test/TestClass.java
@@ -0,0 +1,24 @@
+package com.placementserver.placementserver.test;
+
+public class TestClass {
+	
+	private String helloworld;
+
+	public String getHelloworld() {
+		return helloworld;
+	}
+
+	public void setHelloworld(String helloworld) {
+		this.helloworld = helloworld;
+	}
+	
+	public TestClass() {
+		super();
+	}
+
+	public TestClass(String helloworld) {
+		super();
+		this.helloworld = helloworld;
+	}
+	
+}
